58475e0a5c3173bd86c4b9dd23ca2e53656dbd62
ariyan-king <114898480+ariyan-king@users.noreply.github.com>
2022-10-03 15:33:23 +0600

    Create ariyan3.py

---
+++ ariyan3.py
@@ -0,0 +1,89 @@
+^?ELF^B^A^A^@^@^@^@^@^@^@^@^@^C^@�^@^A^@^@^>
+^@@^@ ^@^^^@^F^@^@^@^D^@^@^@@^@^@^@^@^@^@^@>
+^@^@^R^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^>
+^@^@^R^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^>
+^@^@^R^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^>
+^@^@^R^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^>
+^@^@^Q^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^>
+^@^@^P^@^@^@^@^@^@^@c^M^E^@^@^@^B^@y
+^@^@^@^@^@^@^@^@^@^@^A^@^@^@�^@^@^@^A^@^@^@>
+�^@^@^@�qX^\���|BE���nE��^Z�Z�^@^@^@�^@^@^@>
+^@^@^@T^@^@^@~^@^@^@�^@^@^@�^@^@^@1^@^@^@�^>
+^?^@^@^@^@^@^@�,^C^@^@^@^@^@^C^D^@^@^@^@^@^>
+^@^@^@^@^@^@^@^@^@^@^@�M^C^@^@^@^@^@^B^D^@^>
+██   ██ ████   ██ ██   ██ ████  ████
+
+ ^[[1;33m██████  ██ ██  ██ ██   ██ ██ ████ >
+
+ ██   ██ ██  ██ ██ ██   ██ ██  ██  ██
+
+ ^[[1;36m██   ██ ██   ████ ██████  ██      >
+
+(!)═══════════════════════════════════════
+
+(!) Author   : Ariyan Islam Nil
+
+(!) Guthub   : ariyan-king
+
+(!) Facebook : Ariyan Islam Nil
+
+(!) Type     : Trail
+
+(!) Enjoy Trail Share SS in Group ❤
+^[[1;37m(!)════════════════════════════════>
+
+^[[1;37m(!)════════════════════════════════>
+
+^[[1;37m[2] Random Cloning ^[[1;32m[M2]
+
+^[[1;37m[0] Exit Menu ^@2^@^[[38;5;208m^@6^>
+
+[•] Total OK Ids : ^@ConnectionError^@D^@Di>
+
+[•] Limint : ^@F^@G^@H^@I^@
+
+(!) Info : Something Working Was Wrong^@J^@>
+
+[•] CP IDS SAVE : /sdcard/ARIYAN-CP.txt^@P^@>
+
+[•] BD Code: 880180,880191,880174,etc...
+
+[•] Code : ^@[•] Press Enter To Back Menu ^>
+
+[•] Total CP IDs : ^@U^@V^@ValueError^@W^@X>
+•] BD Code: 880180,880191,880174,etc...
+
+[•] Code : ^@[•] Press Enter To Back Menu ^>
+
+[•] Total CP IDs : ^@U^@V^@ValueError^@W^@X>
+
+^@^M^@; ^@.^@(^@!^@) ^@  ^@^M ^@[^@a^@aa^@a>
+
+return next yielded value or raise StopIter>
+
+return next yielded value or raise StopIter>
+
+    try: Generator = _module.Generator
+
+    except AttributeError: pass
+
+    else: Generator.register(_cython_genera>
+
+if _cython_coroutine_type is not None:
+
+    try: Coroutine = _module.Coroutine
+
+    except AttributeError: pass
+
+^@need more than %zd value%.1s to unpack^@_>
+^@^@��^@^@
+^@^@x�^@^@@
+^@^@^P�^@^@`
+^@^@��^@^@�
+^@^@8�^@^@�
+^@^@x�^@^@�
+^@^@^T�^@^@�
+^@^@��^@^@^@^K^@^@��^@^@ ^K^@^@��^@^@@^K^@^>
+�^L�^N�^P�^R�^T�^V�^X^@^@^@^\^@^@^@^\^A^@^@>
+�^L^@^\^@^@^@\^A^@^@��^@^@�^@^@^@^@L^L^] �^>
+    else: Coroutine.register(_cython_corout>
